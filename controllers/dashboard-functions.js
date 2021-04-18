2ef061b1368705b7123ef35f4ba48598d429cc90
akulczy <agata.kulczy@gmail.com>
2021-04-18 20:39:57 +0100

    Update own job title as administrator

--- controllers/dashboard-functions.js
+++ controllers/dashboard-functions.js
@@ -335,6 +335,7 @@ exports.updateUserDetails = async (req, res) => {
         if(req.body.userSurname != null || req.body.userSurname != "") { user.surname = req.body.userSurname; req.session.surname = req.body.userSurname };
         if(req.body.userEmail != null || req.body.userEmail != "") { user.email = req.body.userEmail; req.session.email = req.body.userEmail };
         if(req.body.userPhone != null || req.body.userPhone != "") { user.telephone = req.body.userPhone };
+        if(req.body.jobTitle != null || req.body.jobTitle != "") { user.title = req.body.jobTitle };
 
         if(req.body.userPassword != null && req.body.userPassword != "") {
             let hashedPass = await bcrypt.hash(req.body.userPassword, 14);
